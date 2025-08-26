acb78d2e44ff74b9c09481aaa76c1620d7f1caf3
Michael Scholten <michael-scholten@hotmail.nl>
2025-08-26 21:15:13 +0200

    Added a progress indicator

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ fn main() {
         let mut out = BufWriter::new(stdout().lock());
         writeln!(out, "P3\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n255\n").unwrap();
         for y in 0..IMAGE_HEIGHT {
+            eprint!("{:02}%\r", y * 100 / IMAGE_HEIGHT);
             for x in 0..IMAGE_WIDTH {
                 let red = (x as f64 / (IMAGE_WIDTH - 1) as f64 * 255.999) as u8;
                 let green = (y as f64 / (IMAGE_HEIGHT - 1) as f64 * 255.999) as u8;
