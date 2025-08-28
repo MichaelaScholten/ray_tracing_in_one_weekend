b95f79bcb2346892f1d8a3f9d0a4f42018a70797
Michael Scholten <michael-scholten@hotmail.nl>
2025-08-28 15:58:23 +0200

    Made metal fuzzy

--- src/main.rs
+++ src/main.rs
@@ -22,25 +22,29 @@ fn main() {
     {
         // Create the world
         let world: Vec<Box<dyn Hittable + Sync>> = vec![
+            // Ground
             Box::new(Sphere::new(
                 Point3::new([0.0, -100.5, -1.0]),
                 100.0,
                 Arc::new(Lambertian::new(Color::new([0.8, 0.8, 0.0]))),
             )),
+            // Center
             Box::new(Sphere::new(
                 Point3::new([0.0, 0.0, -1.2]),
                 0.5,
                 Arc::new(Lambertian::new(Color::new([0.1, 0.2, 0.5]))),
             )),
+            // Left
             Box::new(Sphere::new(
                 Point3::new([-1.0, 0.0, -1.0]),
                 0.5,
-                Arc::new(Metal::new(Color::new([0.8; 3]))),
+                Arc::new(Metal::new(Color::new([0.8; 3]), 0.3)),
             )),
+            // Right
             Box::new(Sphere::new(
                 Point3::new([1.0, 0.0, -1.0]),
                 0.5,
-                Arc::new(Metal::new(Color::new([0.8, 0.6, 0.2]))),
+                Arc::new(Metal::new(Color::new([0.8, 0.6, 0.2]), 1.0)),
             )),
         ];
         let world = HittableList::from(world);
--- src/material/metal.rs
+++ src/material/metal.rs
@@ -1,18 +1,24 @@
-use crate::{color::Color, hittable::HitRecord, material::Material, ray::Ray};
+use crate::{color::Color, hittable::HitRecord, material::Material, ray::Ray, vec3::Vec3};
 
 pub struct Metal {
     albedo: Color,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub const fn new(albedo: Color) -> Self {
-        Self { albedo }
+    pub const fn new(albedo: Color, fuzz: f64) -> Self {
+        Self {
+            albedo,
+            fuzz: fuzz.min(1.0),
+        }
     }
 }
 
 impl Material for Metal {
     fn scatter(&self, ray: &Ray, record: &HitRecord) -> Option<(crate::ray::Ray, Color)> {
-        let reflected = ray.direction().reflect(record.normal());
-        Some((Ray::new(*record.point(), reflected), self.albedo))
+        let reflected = ray.direction().reflect(record.normal()).unit_vector()
+            + (self.fuzz * Vec3::random_unit_vector());
+        let scattered = Ray::new(*record.point(), reflected);
+        (scattered.direction().dot(record.normal()) > 0.0).then_some((scattered, self.albedo))
     }
 }
