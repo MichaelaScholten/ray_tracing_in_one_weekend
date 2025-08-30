15687a702039e97416841e858ce2e896418cd1be
Michael Scholten <michael-scholten@hotmail.nl>
2025-08-30 14:36:37 +0200

    Removed the curly braces around the rendering part in the main function, as it hasn't been needed since the camera was added

--- src/main.rs
+++ src/main.rs
@@ -19,48 +19,46 @@ pub mod vec3;
 fn main() {
     let start = Instant::now();
 
-    {
-        // Create the world
-        let world: Vec<Box<dyn Hittable + Sync>> = vec![
-            // Ground
-            Box::new(Sphere::new(
-                Point3::new([0.0, -100.5, -1.0]),
-                100.0,
-                Arc::new(Lambertian::new(Color::new([0.8, 0.8, 0.0]))),
-            )),
-            // Center
-            Box::new(Sphere::new(
-                Point3::new([0.0, 0.0, -1.2]),
-                0.5,
-                Arc::new(Lambertian::new(Color::new([0.1, 0.2, 0.5]))),
-            )),
-            // Left
-            Box::new(Sphere::new(
-                Point3::new([-1.0, 0.0, -1.0]),
-                0.5,
-                Arc::new(Dielectric::new(1.5)),
-            )),
-            // Bubble
-            Box::new(Sphere::new(
-                Point3::new([-1.0, 0.0, -1.0]),
-                0.4,
-                Arc::new(Dielectric::new(1.0 / 1.5)),
-            )),
-            // Right
-            Box::new(Sphere::new(
-                Point3::new([1.0, 0.0, -1.0]),
-                0.5,
-                Arc::new(Metal::new(Color::new([0.8, 0.6, 0.2]), 1.0)),
-            )),
-        ];
-        let world = HittableList::from(world);
+    // Create the world
+    let world: Vec<Box<dyn Hittable + Sync>> = vec![
+        // Ground
+        Box::new(Sphere::new(
+            Point3::new([0.0, -100.5, -1.0]),
+            100.0,
+            Arc::new(Lambertian::new(Color::new([0.8, 0.8, 0.0]))),
+        )),
+        // Center
+        Box::new(Sphere::new(
+            Point3::new([0.0, 0.0, -1.2]),
+            0.5,
+            Arc::new(Lambertian::new(Color::new([0.1, 0.2, 0.5]))),
+        )),
+        // Left
+        Box::new(Sphere::new(
+            Point3::new([-1.0, 0.0, -1.0]),
+            0.5,
+            Arc::new(Dielectric::new(1.5)),
+        )),
+        // Bubble
+        Box::new(Sphere::new(
+            Point3::new([-1.0, 0.0, -1.0]),
+            0.4,
+            Arc::new(Dielectric::new(1.0 / 1.5)),
+        )),
+        // Right
+        Box::new(Sphere::new(
+            Point3::new([1.0, 0.0, -1.0]),
+            0.5,
+            Arc::new(Metal::new(Color::new([0.8, 0.6, 0.2]), 1.0)),
+        )),
+    ];
+    let world = HittableList::from(world);
 
-        // Create the camera
-        let camera = Camera::new(16.0 / 9.0, 512, 128, 50);
+    // Create the camera
+    let camera = Camera::new(16.0 / 9.0, 512, 128, 50);
 
-        // Use the camera to make a picture of the world
-        camera.render(&world);
-    }
+    // Use the camera to make a picture of the world
+    camera.render(&world);
 
     eprintln!("{:?}", start.elapsed());
 }
